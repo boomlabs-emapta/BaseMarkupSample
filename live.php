af6910ad27c6586bbfe60e3931fd01f1b3d2c846
Christian Diomampo <chrisgen19@gmail.com>
2018-01-24 07:22:42 +0800

    Initial commit

---
+++ live.php
@@ -0,0 +1,417 @@
+<?php require_once('includes/partials/header.php');?>		
+		<div class="content-overlay"></div>
+		
+		<!-- FEATURE -->
+		<section class="feature-panel">
+			<div class="col2-panel cards sixteenByNine margin-0 img-margin-0"><!--
+				--><div class="left-col">
+						<div class="card-slider featureLeftSlider">
+							<div class="swiper-wrapper">
+								<!-- slider 1 -->
+								<div class="card swiper-slide">
+									<div class="img ">
+										<a href="#" data-router="">
+											<img src="http://placehold.it/960x540/673ab7/ffffff?text=SLIDER 1" alt="">
+										</a>
+									</div>
+									
+								</div>
+
+								<!-- slider 2 -->
+								<div class="card swiper-slide">
+									<div class="img ">
+										<a href="#" data-router="">
+											<img src="http://placehold.it/960x540/00bcd4/ffffff?text=SLIDER 2" alt="">
+										</a>
+									</div>
+									
+								</div>
+
+								<!-- slider 3 -->
+								<div class="card swiper-slide">
+									<div class="img ">
+										<a href="#" data-router="">
+											<img src="http://placehold.it/960x540/cddc39/ffffff?text=SLIDER 3" alt="">
+										</a>
+									</div>
+									
+								</div>
+							</div>
+						</div>
+						<div class="previous-feature"><i class="fa fa-8x fa-chevron-left"></i></div>
+						<div class="next-feature"><i class="fa fa-8x fa-chevron-right"></i></div>
+
+				</div><!--
+				--><div class="right-col">
+						<div class="flex-wrapper">
+							<div class="title-bar"><div class="title">Live</div></div>
+							<div class="list">
+								<div class="card-slider featureRightSlider">
+									<div class="swiper-wrapper">
+										<!-- slider 1 -->
+										<div class="card swiper-slide">
+											<div class="img-vertical">
+												<a href="#" data-router="">
+													<img src="http://placehold.it/250x150/673ab7/ffffff?text=SLIDER 1" alt="">
+												</a>
+											</div>
+											<div class="info"> </div>
+											
+										</div>
+
+										<!-- slider 2 -->
+										<div class="card swiper-slide">
+											<div class="img-vertical">
+												<a href="#" data-router="">
+													<img src="http://placehold.it/250x150/00bcd4/ffffff?text=SLIDER 2" alt="">
+												</a>
+											</div>
+											<div class="info">
+												
+											</div>
+										</div>
+
+										<!-- slider 3 -->
+										<div class="card swiper-slide">
+											<div class="img-vertical">
+												<a href="#" data-router="">
+													<img src="http://placehold.it/250x150/cddc39/ffffff?text=SLIDER 3" alt="">
+												</a>
+											</div>
+											<div class="info">
+												
+											</div>
+										</div>
+										<!-- slider 4 -->
+										<div class="card swiper-slide">
+											<div class="img-vertical">
+												<a href="#" data-router="">
+													<img src="http://placehold.it/250x150/ef5160/ffffff?text=SLIDER 4" alt="">
+												</a>
+											</div>
+											<div class="info">
+												
+											</div>
+										</div>
+										<!-- slider 5 -->
+										<div class="card swiper-slide">
+											<div class="img-vertical">
+												<a href="#" data-router="">
+													<img src="http://placehold.it/250x150/cddc39/ffffff?text=SLIDER 5" alt="">
+												</a>
+											</div>
+											<div class="info">
+												
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="footer-bar">
+								<div class="previous"><i class="fa fa-5x fa-chevron-up"></i></div>
+								<div class="next"><i class="fa fa-5x fa-chevron-down"></i></div>
+							</div>
+						</div>			
+				</div><!--
+			--></div>
+		</section>
+		
+		<div class="container bg-grey">
+			<div class="card-container">
+				<h2>Latest</h2>
+				<div class="cards sixteenByNine img-margin-0">
+					<div class="card-slider latestSlider  bg-white">
+							<div class="swiper-wrapper">
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ed3a4b/ffffff.png&amp;text=SLIDER 1" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+									<div class="label label-right">
+										New!
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/00cc00/ffffff.png&amp;text=SLIDER 2" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+									<div class="label label-right">
+										New!
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/3f51b5/ffffff.png&amp;text=SLIDER 3" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+									<div class="label label-right">
+										New!
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ff9800/ffffff.png&amp;text=SLIDER 4" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+									<div class="label label-right">
+										New!
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/009688/ffffff.png&amp;text=SLIDER 5" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+									<div class="label label-right">
+										New!
+									</div>
+								</div>
+							</div>
+					</div>
+					<div class="previous latest-previous">
+						<i class="fa fa-3x fa-chevron-left"></i>
+					</div>
+					<div class="next latest-next">
+						<i class="fa fa-3x fa-chevron-right"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container bg-grey">
+			<div class="card-container">
+				<h2>Movies</h2>
+				<div class="cards cards-bg-white sixteenByNine img-margin-0">
+					<div class="card-slider moviesSlider ">
+							<div class="swiper-wrapper">
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ed3a4b/ffffff.png&amp;text=SLIDER 1" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/00cc00/ffffff.png&amp;text=SLIDER 2" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/3f51b5/ffffff.png&amp;text=SLIDER 3" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ff9800/ffffff.png&amp;text=SLIDER 4" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/009688/ffffff.png&amp;text=SLIDER 5" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+					</div>
+					<div class="previous movies-previous">
+						<i class="fa fa-3x fa-chevron-left"></i>
+					</div>
+					<div class="next movies-next">
+						<i class="fa fa-3x fa-chevron-right"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container bg-grey">
+			<div class="card-container">
+				<h2>Promos</h2>
+				<div class="cards cards-bg-white sixteenByNine img-margin-0">
+					<div class="card-slider promosSlider ">
+							<div class="swiper-wrapper">
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ed3a4b/ffffff.png&amp;text=SLIDER 1" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/00cc00/ffffff.png&amp;text=SLIDER 2" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/3f51b5/ffffff.png&amp;text=SLIDER 3" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/ff9800/ffffff.png&amp;text=SLIDER 4" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card swiper-slide">
+									<div class="img">
+										<img src="http://placehold.it/960x540/009688/ffffff.png&amp;text=SLIDER 5" alt="">
+									</div>
+									<div class="info">
+										<div class="info-container">
+											<div class="titles">
+												<a href="#" data-router="">
+												<h3 class="no-wrap">Subcaegory Title</h3>
+												<h2 class="title no-wrap">Episode #</h2>
+											</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+					</div>
+					<div class="previous promos-previous">
+						<i class="fa fa-3x fa-chevron-left"></i>
+					</div>
+					<div class="next promos-next">
+						<i class="fa fa-3x fa-chevron-right"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+
+		
+		
+
+
+		
+	<?php require_once('includes/partials/footer.php');?>
